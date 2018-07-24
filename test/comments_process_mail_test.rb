d85a9aefab663ae6424cf74cbd3f2d5a69d587eb
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-07-24 11:25:17 -0400

    Move some test code

--- test/comments_process_mail_test.rb
+++ test/comments_process_mail_test.rb
@@ -2,47 +2,9 @@
 
 require_relative 'test_helper'
 require_relative 'test_helper_methods'
+require_relative 'comments_process_impure_email_send_class_methods'
 require 'email_send'
 require 'my_file'
-
-module ::CommentsProcess
-  module Impure
-    module EmailSend
-      module ClassMethods
-
-## Keep before attr_reader:
-        def self.names_ordered_test
-          a = %w[
-              email
-              address
-              address_reply_to
-              password
-              ]
-          a.map{|e| :"#{e}_test"}
-        end
-
-        attr_reader(*names_ordered_test)
-
-        def connect_and_send(*args)
-          a = my_self.names_ordered_test
-          a.zip(args).each do         |name,   arg|
-            instance_variable_set :"@#{name}", arg
-          end
-        end
-
-        def my_self
-## Here, simply using 'self' doesn't work. (I don't know why.)
-## Clue: 'self' is 'CommentsProcess::Impure::EmailSend'.
-## Clue; 'CommentsProcess::Impure::EmailSend' is extended with
-##   '::CommentsProcess::Impure::EmailSend::ClassMethods',
-##   (rather than including it).
-#-------------
-         ::CommentsProcess::Impure::EmailSend::ClassMethods
-        end
-      end
-    end
-  end
-end
 
 module ::QplaylistRememberCommentsProcessTest
   class CommentsProcessMailTest < CommentsProcessTest
