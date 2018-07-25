3db4adee192e394d1238e010000cecc1be23303f
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-07-25 08:51:48 -0400

    Use module name

--- test/comments_process/test_fixture_data.rb
+++ test/comments_process/test_fixture_data.rb
@@ -2,24 +2,26 @@
 
 require 'period_record'
 
-module ::CommentsProcess
-  class TestFixtureData
+module ::QplaylistRememberCommentsProcess
+  module ::CommentsProcess
+    class TestFixtureData
 
-    attr_reader :value
+      attr_reader :value
 
-    def initialize
-      period_array = %w[
-          sun
-          17
-          19
-          bob
-          bobsmith@example.com
-          ]
-      period_line = period_array.join ' '
-      period = Pure::PeriodRecord.new period_line
-      comments = ::Array.new
-      data = period, comments
-      @value = data
+      def initialize
+        period_array = %w[
+            sun
+            17
+            19
+            bob
+            bobsmith@example.com
+            ]
+        period_line = period_array.join ' '
+        period = Pure::PeriodRecord.new period_line
+        comments = ::Array.new
+        data = period, comments
+        @value = data
+      end
     end
   end
 end
--- test/comments_process_impure_email_send_class_methods.rb
+++ test/comments_process_impure_email_send_class_methods.rb
@@ -1,40 +1,42 @@
 # coding: utf-8
 
-module ::CommentsProcess
-  module Impure
-    module EmailSend
-      module ClassMethods
+module ::QplaylistRememberCommentsProcess
+  module ::CommentsProcess
+    module Impure
+      module EmailSend
+        module ClassMethods
 
 ## Keep before attr_reader:
-        def self.names_ordered_test
-          a = %w[
-              email
-              address
-              address_reply_to
-              password
-              ]
-          a.map{|e| :"#{e}_test"}
-        end
-
-        attr_reader(*names_ordered_test)
-
-        def connect_and_send(*args)
-          a = my_self.names_ordered_test
-          a.zip(args).each do         |name,   arg|
-            instance_variable_set :"@#{name}", arg
+          def self.names_ordered_test
+            a = %w[
+                email
+                address
+                address_reply_to
+                password
+                ]
+            a.map{|e| :"#{e}_test"}
           end
-        end
 
-        private
+          attr_reader(*names_ordered_test)
 
-        def my_self
+          def connect_and_send(*args)
+            a = my_self.names_ordered_test
+            a.zip(args).each do         |name,   arg|
+              instance_variable_set :"@#{name}", arg
+            end
+          end
+
+          private
+
+          def my_self
 ## Here, simply using 'self' doesn't work. (I don't know why.)
 ## Clue: 'self' is 'CommentsProcess::Impure::EmailSend'.
 ## Clue; 'CommentsProcess::Impure::EmailSend' is extended with
 ##   '::CommentsProcess::Impure::EmailSend::ClassMethods',
 ##   (rather than including it).
 #-------------
-         ::CommentsProcess::Impure::EmailSend::ClassMethods
+           ::CommentsProcess::Impure::EmailSend::ClassMethods
+          end
         end
       end
     end
