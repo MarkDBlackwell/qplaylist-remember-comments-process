af12b853e7fd46f2b86566504e8b31f6c365e80d
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-07-24 20:30:43 -0400

    Improve

--- lib/comments_process/pure/comments_by_timestamp_hash.rb
+++ lib/comments_process/pure/comments_by_timestamp_hash.rb
@@ -1,30 +1,11 @@
 # coding: utf-8
 
+require 'comments_by_timestamp_hash_instance_methods'
+
 module ::CommentsProcess
   module Pure
     class CommentsByTimestampHash
-
-      include ::Enumerable
-
-      def initialize(period_comments)
-        @comments_by_timestamp_hash = comments_by_timestamp_hash period_comments
-      end
-
-      def each
-        unless block_given?
-          @comments_by_timestamp_hash.each
-        else
-## 'each(&:yield)' doesn't work, here.
-          @comments_by_timestamp_hash.each{|e| yield e}
-        end
-      end
-
-      private
-
-      def comments_by_timestamp_hash(period_comments)
-        grouped = period_comments.group_by(&:timestamp)
-        grouped.sort.to_h
-      end
+      include ::CommentsProcess::Pure::CommentsByTimestampHash::InstanceMethods
     end
   end
 end
---
+++ lib/comments_process/pure/comments_by_timestamp_hash_instance_methods.rb
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+module ::CommentsProcess
+  module Pure
+    class CommentsByTimestampHash
+      module InstanceMethods
+
+        include ::Enumerable
+
+        def initialize(period_comments)
+          @comments_by_timestamp_hash = comments_by_timestamp_hash period_comments
+        end
+
+        def each
+          unless block_given?
+            @comments_by_timestamp_hash.each
+          else
+## 'each(&:yield)' doesn't work, here.
+            @comments_by_timestamp_hash.each{|e| yield e}
+          end
+        end
+
+        private
+
+        def comments_by_timestamp_hash(period_comments)
+          grouped = period_comments.group_by(&:timestamp)
+          grouped.sort.to_h
+        end
+      end
+    end
+  end
+end
